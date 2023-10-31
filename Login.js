3fde51e4497e84e20d5eca41ba04cf0a1a668ef5
sonkaru <kurmetsunkar0407@gmail.com>
2023-10-31 23:41:12 +0600

    login done

--- Login.js
+++ Login.js
@@ -32,11 +32,50 @@ login.addEventListener("click", ( )=>{
 });
 signup.addEventListener("click", () => {
   container.classList.add("active");
-  alert("Success"); 
+
 });
 
 logins.addEventListener("click", () => {
   container.classList.remove("active");
   audioPlayer.play();
-  alert("Success"); 
+
 });
+
+function register() {
+  const checkPassword =  document.getElementById('checkPassword').value;
+  const username = document.getElementById('getUsername').value;
+  const userAccount = document.getElementById('registerUsername').value;
+  const password = document.getElementById('registerPassword').value;
+
+if(checkPassword === password){
+  if (localStorage.getItem(userAccount) !== null) {
+      alert('User already exists. Please choose a different username.');
+  } else {
+      localStorage.setItem(userAccount, password,username);
+      alert('Registration successful. You can now log in.');
+  }
+}
+else{
+  alert('password are not same')
+}
+}
+
+function userLogin() {
+  const username = document.getElementById('getUsername').value;
+  const userAccount= document.getElementById('loginUsername').value;
+  const password = document.getElementById('loginPassword').value;
+
+  if (localStorage.getItem(userAccount) === password&&localStorage.getItem(userAccount)!=='') {
+  
+    alert('Login successful. Welcome, ' + username + '!');
+      window.location.href = "/Websites/Podcasts.html";
+
+  } 
+ 
+  else {
+      alert('Invalid username or password.');
+  }
+}
+
+
+
